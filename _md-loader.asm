911d28bb763b11b69bee874a81ce3a03ac39624e
Maciej Małecki <external.maciej.malecki@kuehne-nagel.com>
2024-05-05 13:20:08 +0200

    lic

--- _md-loader.asm
+++ _md-loader.asm
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2023 Maciej Małecki
+ * Copyright (c) 2024 Maciej Małecki
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,7 +21,6 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
-
 #importonce 
 
 .macro createMagicDeskLoader() {
